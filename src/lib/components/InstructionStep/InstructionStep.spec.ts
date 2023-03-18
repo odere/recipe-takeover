57640beda673b69c94723f551b67e3ba8219a204
odere <aleksandr.derechei@gmail.com>
2023-03-19 00:28:09 +0100

    add InstructionStep unit tests

---
+++ src/lib/components/InstructionStep/InstructionStep.spec.ts
@@ -0,0 +1,55 @@
+import { render, fireEvent, screen } from '@testing-library/svelte';
+
+import InstructionStep from './InstructionStep.svelte';
+
+describe('Test Ingredient.svelte', async () => {
+	const props = {
+		instruction: 'instruction',
+	};
+
+	it('Should render ingredient data', () => {
+		render(InstructionStep, props);
+
+		expect(screen.getByText(props.instruction as string)).toBeInTheDocument();
+	});
+
+	it('Should check/uncheck by clicking on checkbox', async () => {
+		render(InstructionStep, props);
+
+		expect(document.querySelector('.checked')).not.toBeInTheDocument();
+
+		let checkbox = document.querySelector('input');
+		if (checkbox) {
+			await fireEvent.click(checkbox);
+		}
+
+		expect(document.querySelector('.checked')).toBeInTheDocument();
+
+		checkbox = document.querySelector('input');
+		if (checkbox) {
+			await fireEvent.click(checkbox);
+		}
+
+		expect(document.querySelector('.checked')).not.toBeInTheDocument();
+	});
+
+	it('Should check/uncheck by clicking on checkbox label(component wrapper)', async () => {
+		render(InstructionStep, props);
+
+		expect(document.querySelector('.checked')).not.toBeInTheDocument();
+
+		let label = document.querySelector('label');
+		if (label) {
+			await fireEvent.click(label);
+		}
+
+		expect(document.querySelector('.checked')).toBeInTheDocument();
+
+		label = document.querySelector('label');
+		if (label) {
+			await fireEvent.click(label);
+		}
+
+		expect(document.querySelector('.checked')).not.toBeInTheDocument();
+	});
+});
